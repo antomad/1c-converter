7a83b1d35cf06d86d2b2980383c6b2c6593be0a1
anton.shurtakov <anton@shurtakov.ru>
2022-09-06 22:06:16 +0300

    added spring boot application

---
+++ src/test/java/ru/antomad/cconverter/ApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.antomad.cconverter;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("No build-time integration test")
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
